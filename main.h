12691604bfcc9c7f50d3a6886007c65244f8bda2
Oyindamola Dawodu <kudiratoyindamolad@gmail.com>
2022-05-22 14:47:57 +0000

    main.h

--- main.h
+++ main.h
@@ -1,76 +1,23 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdlib.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 #include <stdarg.h>
 
-/**
- *struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
- */
-typedef struct flags
-{
-	int plus;
-	int space;
-	int hash;
-} flags_t;
-
-/**
- * struct printHandler - struct to choose the right function depending
- * on the format specifier passed to _printf()
- * @c: format specifier
- * @f: pointer to the correct printing function
- */
-typedef struct printHandler
-{
-	char c;
-	int (*f)(va_list ap, flags_t *f);
-} ph;
-
-/* print_nums */
-int print_int(va_list l, flags_t *f);
-void print_number(int n);
-int print_unsigned(va_list l, flags_t *f);
-int count_digit(int i);
-
-/* print bases */
-int print_hex(va_list l, flags_t *f);
-int print_hex_big(va_list l, flags_t *f);
-int print_binary(va_list l, flags_t *f);
-int print_octal(va_list l, flags_t *f);
-
-/* converter */
-char *convert(unsigned long int num, int base, int lowercase);
-
-/* _printf */
-int _printf(const char *format, ...);
-
-/* get_print */
-int (*get_print(char s))(va_list, flags_t *);
-
-/* get_flag */
-int get_flag(char s, flags_t *f);
-
-/* print_alpha */
-int print_string(va_list l, flags_t *f);
-int print_char(va_list l, flags_t *f);
-
-/* write_funcs */
 int _putchar(char c);
-int _puts(char *str);
+int _printf(const char *format, ...);
+int _print_int(va_list args);
+int _print_char(va_list args);
+int _print_str(va_list args);
+int _print_bin(va_list args);
+int _print_dec(va_list args);
+int _print_perc(va_list args);
+int (*_select_func(const char c))(va_list);
+int _print_octal(va_list args);
+int _print_unint(va_list args);
+int _print_x(va_list args);
+int _print_X(va_list args);
+int _print_hex(unsigned int n, unsigned int c);
+int _print_Rev(va_list args);
+int _print_rot(va_list args);
 
-/* print_custom */
-int print_rot13(va_list l, flags_t *f);
-int print_rev(va_list l, flags_t *f);
-int print_bigS(va_list l, flags_t *f);
-
-/* print_address */
-int print_address(va_list l, flags_t *f);
-
-/* print_percent */
-int print_percent(va_list l, flags_t *f);
 
 #endif
